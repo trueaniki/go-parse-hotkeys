b45055af70f2fea5a62e42a6b735a6fcf3d6efc7
Aniki <anikipelau@gmail.com>
2024-03-07 02:52:27 +0300

    add tests

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/trueaniki/go-parse-hotkeys
 
 go 1.19
 
-require golang.design/x/hotkey v0.4.1 // indirect
+require golang.design/x/hotkey v0.4.1
--- parse_test.go
+++ parse_test.go
@@ -1,10 +1,10 @@
 package parsehotkeys_test
 
 import (
-	"fmt"
 	"testing"
 
 	parsehotkeys "github.com/trueaniki/go-parse-hotkeys"
+	"golang.design/x/hotkey"
 )
 
 func TestParse(t *testing.T) {
@@ -13,19 +13,25 @@ func TestParse(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("Hotkey: %s\n", hk)
+	if hk.String() != hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyA).String() {
+		t.Error("Test failed")
+	}
 
 	input = "CTRL alt a"
 	hk, err = parsehotkeys.Parse(input, " ")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("Hotkey: %s\n", hk)
+	if hk.String() != hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModOption}, hotkey.KeyA).String() {
+		t.Error("Test failed")
+	}
 
 	input = "Ctrl&Shift&A"
 	hk, err = parsehotkeys.Parse(input, "&")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("Hotkey: %s\n", hk)
+	if hk.String() != hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyA).String() {
+		t.Error("Test failed")
+	}
 }
